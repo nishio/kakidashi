eb51ec8df4bf4fc3c7061bfe43edf381a58c5a19
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-02-20 18:38:52 +0900

    add functions

---
+++ functions/makeManifest.js
@@ -0,0 +1,34 @@
+let base = {
+  "short_name": "React App",
+  "name": "Create React App Sample",
+  "icons": [
+    {
+      "src": "favicon.ico",
+      "sizes": "64x64 32x32 24x24 16x16",
+      "type": "image/x-icon"
+    },
+    {
+      "src": "logo192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "logo512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": ".",
+  "display": "standalone",
+  "theme_color": "#000000",
+  "background_color": "#ffffff"
+}
+
+exports.handler = function (event, context, callback) {
+  start_url = event.queryStringParameters
+  base.start_url = start_url
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(base)
+  })
+}
